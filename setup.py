80a7b919e3cc42a3a0ad2daf6f02cb0f576926ec
Maxsmile123 <sysoevmaksim27@gmail.com>
2022-07-07 01:38:54 +0300

    add download video

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-CODE_DIR = 'E2FGVI'
+CODE_DIR = 'Third_party/E2FGVI'
 os.makedirs(f'./{CODE_DIR}')
 os.system("cd " + CODE_DIR)
 os.system("git clone https://github.com/MCG-NKU/E2FGVI.git")

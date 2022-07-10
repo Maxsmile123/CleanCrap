8308431b53c08e6c1d11af28286c0196d8f750b0
alpnva <74810136+alpnva@users.noreply.github.com>
2022-07-10 19:20:39 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-import os
-
-CODE_DIR = 'Third_party/E2FGVI'
-os.makedirs(f'./{CODE_DIR}')
-os.system("cd " + CODE_DIR)
-os.system("git clone https://github.com/MCG-NKU/E2FGVI.git")
-os.chdir(f'./{CODE_DIR}')
